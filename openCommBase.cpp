b540c032a2e31c0f8478332061e636c22ddfa7ba
Kevin <zhaigangvvv@126.com>
2016-12-14 07:32:28 +0800

    Add new method

--- openCommBase.cpp
+++ openCommBase.cpp
@@ -27,20 +27,41 @@ int openCommBase::createOpenCLEnv(void)
     if (CL_SUCCESS != ret)
     {
       cout << "Error getting GPU device number:" << ret << endl;
+      return 0;
     }
     cl_device_id device_id;
     ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 0, &device_id, NULL);
     if (CL_SUCCESS != ret)
     {
       cout << "Error getting GPU device id:" << ret << endl;
+      return 0;
     }
     //Create context.
     cl_context_properties prop[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)platform_id, 0};
-    cl_context context;
-    context = clCreateContext(prop, 0, &device_id, NULL, NULL, &ret);
-    if (CL_SUCCESS != ret)
+    ptr.cc = clCreateContext(prop, 1, &device_id, NULL, NULL, &ret);
+    if (CL_SUCCESS != ret || ptr.cc == NULL)
     {
       cout << "Error getting context:" << ret << endl;
+      return 0;
+    }
+    //Create command queue.
+    ptr.ccq = clCreateCommandQueue(ptr.cc, device_id, 0, &ret);
+    if (CL_SUCCESS != ret || ptr.ccq == NULL)
+    {
+      cout << "Error getting command queue:" << ret << endl;
+      return 0;
+    }
+    //Create memory obj.
+    int * host_ptr = NULL;
+    int arr_size = 1000;
+     
+    host_ptr = (int *)malloc(arr_size*sizeof(int));
+    memcpy(host_ptr, 0, arr_size*sizeof(int));
+    ptr.cm = clCreateBuffer(ptr.cc, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR, arr_size*sizeof(int), host_ptr, &ret);
+    if (CL_SUCCESS != ret || ptr.cm == NULL)
+    {
+      cout << "Error getting mem obj:" << ret << endl;
+      return 0;
     }
 }
 
--- openCommBase.hpp
+++ openCommBase.hpp
@@ -2,12 +2,21 @@
 #define __OPENCOMMBASE__HPP__
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <CL/cl.h>
+
+typedef struct{
+  cl_command_queue ccq;
+  cl_context cc;
+  cl_mem cm;
+}CL_PTR;
 
 class openCommBase{
 public: 
 	//openCommBase();
 	int createOpenCLEnv();
 	int getPlatformId();
+private:
+	CL_PTR ptr;
 };
 #endif
