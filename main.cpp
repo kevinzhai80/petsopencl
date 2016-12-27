e71e241c8dd20e9f0dc36556a637d96501f6e811
kevin <zhaigangvvv@126.com>
2016-12-27 13:57:32 +0800

    Modify executing condition for segment error

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "openCommBase.hpp"
-int main()
-{
+int main(){
     openCommBase cl_base;
     cl_uint work_dim = 2;
     size_t g_work_offect[] = {0, 0, 0};
@@ -12,11 +11,11 @@ int main()
     "int index = get_global_id(0)\n"
     "sum[index] += sum[index]\n"
     "}\n";
-    if (0 != cl_base.createOpenCLEnv())
-    {
-      cl_base.processKernelProgram(kernel_source, "test");
-      cl_base.eqNDRangeKernel(work_dim, g_work_offect,
-      g_work_size, l_work_size);
+    if (0 < cl_base.createOpenCLEnv()){
+      if (0 < cl_base.processKernelProgram(kernel_source, "test")){
+        cl_base.eqNDRangeKernel(work_dim, g_work_offect,
+        g_work_size, l_work_size);
+      }
     }
     return 0;
 }
--- openCommBase.cpp
+++ openCommBase.cpp
@@ -5,32 +5,32 @@ using namespace std;
 
 int openCommBase::createOpenCLEnv(void)
 {
-    cl_int ret;
+    cl_int ret = 0;
     //Get platform number and ID.
     ret = clGetPlatformIDs(0, NULL, &num_platform);
     if (CL_SUCCESS == ret || num_platform < 1)
     {
       cout << "Error getting platform number:" << ret << endl;
-      return 0;
+      return -1;
     }
     ret = clGetPlatformIDs(0, &platform_id, NULL);
     if (CL_SUCCESS != ret)
     {
       cout << "Error getting platform id:" << ret << endl;
-      return 0;
+      return -1;
     }
     //Get device number and ID.
     ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 0, NULL, &num_device);
     if (CL_SUCCESS != ret)
     {
       cout << "Error getting GPU device number:" << ret << endl;
-      return 0;
+      return -1;
     }
     ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 0, &device_id, NULL);
     if (CL_SUCCESS != ret)
     {
       cout << "Error getting GPU device id:" << ret << endl;
-      return 0;
+      return -1;
     }
     //Create context.
     cl_context_properties prop[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)platform_id, 0};
@@ -38,14 +38,14 @@ int openCommBase::createOpenCLEnv(void)
     if (CL_SUCCESS != ret || cc == NULL)
     {
       cout << "Error getting context:" << ret << endl;
-      return 0;
+      return -1;
     }
     //Create command queue.
     ccq = clCreateCommandQueue(cc, device_id, 0, &ret);
     if (CL_SUCCESS != ret || ccq == NULL)
     {
       cout << "Error getting command queue:" << ret << endl;
-      return 0;
+      return -1;
     }
     //Create memory obj.
     int * host_ptr = NULL;
@@ -57,50 +57,53 @@ int openCommBase::createOpenCLEnv(void)
     if (CL_SUCCESS != ret || cm == NULL)
     {
       cout << "Error getting mem obj:" << ret << endl;
-      return 0;
+      return -1;
     }
     free(host_ptr);
+    return ret;
 }
 
 int openCommBase::processKernelProgram(char *ksource, char *kname)
 {
-    cl_int ret;
+    cl_int ret = 0;
     //Create program.
     cp = clCreateProgramWithSource(cc, 1, (const char **)&ksource, NULL, &ret);
     if (CL_SUCCESS != ret || cp == NULL)
     {
       cout << "Error create kernel program:" << ret << endl;
-      return 0;
+      return -1;
     }
     ret = clBuildProgram(cp, 1, &device_id, NULL, NULL, NULL);
     if (CL_SUCCESS != ret)
     {
       cout << "Error build program:" << ret << endl;
-      return 0;
+      return -1;
     }
     ck = clCreateKernel(cp, kname, &ret);
     if (ret != CL_SUCCESS || ck == NULL)
     {
       cout << "Error create kernel:" << ret << endl;
-      return 0;
+      return -1;
     }
     ret = clSetKernelArg(ck, 0, sizeof(cl_mem), (void*)&cm);
     if (CL_SUCCESS != ret)
     {
       cout << "Error set kernel arg:" << ret << endl;
-      return 0;
+      return -1;
     }
+    return ret;
 }
 
 int openCommBase::eqNDRangeKernel(cl_uint work_dim, size_t *g_work_offect, size_t *g_work_size, size_t *l_work_size)
 {
-    cl_int ret;
+    cl_int ret = 0;
     clEnqueueNDRangeKernel(ccq, ck, work_dim, g_work_offect, g_work_size, l_work_size, 0, NULL, NULL);
     if (ret != CL_SUCCESS)
     {
       cout << "Error enqueue kenrel:" << ret << endl;
-      return 0;
+      return -1;
     }
+    return ret;
 }
 
 void openCommBase::release()
